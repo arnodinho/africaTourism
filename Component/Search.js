57294522da1a66ab733ef92a9497940143708e9a
ayangaesso <arnaud.yangaesso@id-apps.fr>
2018-07-11 17:49:37 +0200

    add items

--- Component/Search.js
+++ Component/Search.js
@@ -1,8 +1,10 @@
 // Components/Search.js
 // Avant de pouvoir utiliser les components React Native, il faut les importer de la librairie du même nom
 import React from 'react'
-import { StyleSheet,View, TextInput, Button } from 'react-native'
+import { StyleSheet,View, TextInput,Text, Button,FlatList } from 'react-native'
 
+import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem'
 
 class Search extends React.Component {
 
@@ -10,10 +12,17 @@ class Search extends React.Component {
   render() {
       return (
           // Ici on rend à l'écran les éléments graphiques de notre component custom Search
-          <View style={styles.viewStyle}>
+          <View style={styles.main_container}>
             <TextInput style={styles.textinput} placeholder='Titre du film'/>
             {/* onPress={() => {}} equivaut a onPress={function() {}} */}
             <Button style={styles.buttoninput}  title='Rechercher' onPress={() => {}}/>
+            {/* syntaxe ES6 renderItem={function ({item}) { return <Text>{item.title}</Text> }} */}
+            {/* keyExtractor tells the list to use the ids for the react keys instead of the default key property.  */}
+            <FlatList
+                data={films}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({item}) => <FilmItem/>}/>
+                {/* Lorsque vous définissez une key, React peut rapidement identifier de manière unique un item et faire l'action adéquate : ajout, suppression voir modification */}
           </View>
       )
     }
@@ -21,22 +30,21 @@ class Search extends React.Component {
 
 const styles = StyleSheet.create({
 
-    viewStyle:{
-       marginTop: 60,
-       marginLeft: 25,
-       marginRight: 25,
+    main_container: {
+      flex: 1,
+      marginTop: 60,
     },
-     textinput: {
-       backgroundColor: '#f3f3f3',
-       marginBottom: 15,
-       height: 40,
-       borderColor: '#000000',
-       borderWidth: 1,
-       paddingLeft: 5
-     },
+    textinput: {
+      marginLeft: 5,
+      marginRight: 5,
+      height: 50,
+      borderColor: '#000000',
+      borderWidth: 1,
+      paddingLeft: 5
+    },
 
      buttoninput: {
-       height: 50
+      height: 50,
      }
 })
 // On exporte toujours par défaut les components
